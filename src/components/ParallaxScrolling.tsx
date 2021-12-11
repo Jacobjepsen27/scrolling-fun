b9e78267418c9fdc9b82a64961b6e9fbb6eb7221
Jacob Worck Jepsen <Jwj@trifork.com>
2021-12-11 19:49:20 +0100

    Animation without scroll hook.

--- src/components/ParallaxScrolling.tsx
+++ src/components/ParallaxScrolling.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
-import React, { useLayoutEffect, useRef } from "react";
-import {useScroll} from "../hooks/useScroll";
+import React, { useEffect, useRef } from "react";
 import {Position} from "../models/Position";
 
 const ParallaxBody = styled.div`
@@ -60,12 +59,13 @@ const positions: Position[] = [
 
 
 export const ParallaxScrolling = () => {
-    const { scrollOffset } = useScroll()
+    // const { scrollOffset } = useScroll()
 
     return <ParallaxBody>
         { positions.map((position,index) =>
-            <ParallaxImage key={index} position={position} scrollOffset={scrollOffset}/>
+            <ParallaxImage key={index} position={position} />
         )}
+        {/*<ParallaxImage position={positions[0]} />*/}
 
     </ParallaxBody>
 }
@@ -81,28 +81,31 @@ const ImageDiv = styled.div`
 
 interface ParallaxImageProps {
     position: Position,
-    scrollOffset: number
 }
-const ParallaxImage = ({position, scrollOffset}: ParallaxImageProps) => {
+const ParallaxImage = ({position}: ParallaxImageProps) => {
     const elementRef = useRef<HTMLDivElement | null>(null)
 
-    useLayoutEffect(() => {
-        if(elementRef != null && elementRef.current != null) {
+    const requestRef = useRef(0)
+
+    useEffect(() => {
+        if(elementRef.current != null) {
+
             const updateElementPosition = () => {
                 let _width = window.innerWidth
                 let _height = window.innerHeight
-                let _currentScrollPercentage = scrollOffset / (document.body.scrollHeight - window.innerHeight)
-
+                let _currentScrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight)
                 const [x,y] = calculateNewCoordinates(position, _currentScrollPercentage)
                 const xPixel = x*_width
                 const yPixel = y*_height
 
-                elementRef.current!!.style.transform = `translate3d(${xPixel}px, ${yPixel+scrollOffset}px, 0px)`
+                elementRef.current!!.style.transform = `translate3d(${xPixel}px, ${yPixel+window.scrollY}px, 0px)`
+                requestRef.current = window.requestAnimationFrame(updateElementPosition)
             }
-            updateElementPosition()
-        }
+            requestRef.current = window.requestAnimationFrame(updateElementPosition)
 
-    },[elementRef.current, scrollOffset])
+        }
+        return () => window.cancelAnimationFrame(requestRef.current)
+    },[elementRef.current, position])
 
 
     return <ImageDiv ref={elementRef} />
@@ -110,20 +113,17 @@ const ParallaxImage = ({position, scrollOffset}: ParallaxImageProps) => {
 
 // Finds coordinates on the line based according to percentage
 const calculateNewCoordinates = (position: Position, scrollPercentage: number): [x: number, y: number] => {
-
     if(scrollPercentage <= position.start.percent){
         return [position.start.x,position.start.y]
     }else if (scrollPercentage >= position.end.percent){
         return [position.end.x, position.end.y]
     }else {
         const animationProgressPercentage = calculateAnimationProgressPercentage(position, scrollPercentage)
-        console.log(animationProgressPercentage)
         return [
             position.start.x+(position.end.x-position.start.x)*animationProgressPercentage,
             position.start.y+(position.end.y - position.start.y)*animationProgressPercentage
         ]
     }
-
 }
 
 const calculateAnimationProgressPercentage = (position: Position, scrollPercentage: number): number => {
